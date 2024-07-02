1630c09e9e973414135ed22dcdab7a65139a5f5c
Karabosithole <sitholekarabo0@gmail.com>
2024-07-02 19:27:23 +0200

    Added user class

--- src/main/java/com/github/karabosithole/interactivestory/user/User.java
+++ src/main/java/com/github/karabosithole/interactivestory/user/User.java
@@ -3,4 +3,26 @@ package com.github.karabosithole.interactivestory.user;
  * holds user-specific data.
 */
 public class User {
+    private String currentNode;
+    private int score;
+
+    // Getters and Setters
+
+    public String getCurrentNode() {
+        return currentNode;
+    }
+
+    public void setCurrentNode(String currentNode) {
+        this.currentNode = currentNode;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    // Method to update user's state
 }
