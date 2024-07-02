df139f4092a9ffb338e1e6ce28e81ba479c76aa0
Karabosithole <sitholekarabo0@gmail.com>
2024-07-02 19:26:32 +0200

    Added question bank class

--- src/main/java/com/github/karabosithole/interactivestory/question/QuestionBank.java
+++ src/main/java/com/github/karabosithole/interactivestory/question/QuestionBank.java
@@ -4,5 +4,20 @@ package com.github.karabosithole.interactivestory.question;
  providing methods to retrieve questions randomly or sequentially.
  */
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class QuestionBank {
+    private Map<String, Question> questions = new HashMap<>();
+
+    public void addQuestion(Question question) {
+        questions.put(question.getId(), question);
+    }
+
+    public Question getQuestion(String id) {
+        return questions.get(id);
+    }
+
+    // Method to load questions from YAML (implementation to be added)
 }
