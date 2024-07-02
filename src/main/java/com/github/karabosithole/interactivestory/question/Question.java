de45ccb73ec93f008e56a0792aafc8331e50a0c5
Karabosithole <sitholekarabo0@gmail.com>
2024-07-02 19:24:54 +0200

    added question class

--- src/main/java/com/github/karabosithole/interactivestory/question/Question.java
+++ src/main/java/com/github/karabosithole/interactivestory/question/Question.java
@@ -5,5 +5,59 @@ package com.github.karabosithole.interactivestory.question;
  * that users may encounter at certain points in the story.
  */
 
+import java.util.List;
+
 public class Question {
+    private String id;
+    private String text;
+    private List<Choice> choices;
+
+    // Getters and Setters
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public List<Choice> getChoices() {
+        return choices;
+    }
+
+    public void setChoices(List<Choice> choices) {
+        this.choices = choices;
+    }
+
+    public static class Choice {
+        private String text;
+        private boolean correct;
+
+        // Getters and Setters
+
+        public String getText() {
+            return text;
+        }
+
+        public void setText(String text) {
+            this.text = text;
+        }
+
+        public boolean isCorrect() {
+            return correct;
+        }
+
+        public void setCorrect(boolean correct) {
+            this.correct = correct;
+        }
+    }
 }
